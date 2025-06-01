9b1debb59fdf7b3e5145c47e0c11a0ce3cee1f98
DorianABDS <dorian.abbadessa@laplateforme.io>
2025-06-01 23:52:19 +0200

    feat(core/os): update DEBIAN command to include git submodule initialization

--- core/os/helper.lua
+++ core/os/helper.lua
@@ -1,6 +1,6 @@
 return {
     DEBIAN = {
-        COMMAND = "sudo apt update -y && sudo apt upgrade -y && sudo apt install -y %s",
+        COMMAND = "sudo apt update -y && sudo apt upgrade -y && sudo apt install -y %s && git submodule update --init",
         PACKAGES = "git liblua5.4-dev screen build-essential libreadline-dev unzip clang cmake make gcc g++ libmysqlclient-dev libssl-dev libbz2-dev libreadline-dev libncurses-dev libboost-all-dev p7zip"
     }
 }

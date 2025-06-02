ac0a436baa4f97f832d83a03f48c201e132a8f0b
DorianABDS <dorian.abbadessa@laplateforme.io>
2025-06-02 02:25:26 +0200

    feat(core/cli): implement CommandLineInterpreter

--- core/os/base.lua
+++ core/os/base.lua
@@ -1,3 +1,4 @@
+local Helper = require("core.os.helper")
 local OsExecute = {}
 
 function OsExecute.GetSystemId()
@@ -5,7 +6,6 @@ function OsExecute.GetSystemId()
     if (not os_release_file) then
         return nil
     end
-
 
     local os_info = os_release_file:read("*all")
     os_release_file:close()
@@ -15,6 +15,47 @@ end
 function OsExecute.Run(command)
     local success, result = pcall(os.execute, command)
     return success and result
+end
+
+function OsExecute.GetRootDirectory()
+    local handle
+    if package.config:sub(1,1) == '\\' then
+        -- Windows
+        handle = io.popen("cd")
+    else
+        -- Unix/Linux/MacOS
+        handle = io.popen("pwd")
+    end
+    
+    if handle then
+        local result = handle:read("*a")
+        handle:close()
+
+        result = result:gsub("[\r\n]+$", "")
+        return result
+    end
+    return nil
+end
+
+function OsExecute.GoToDirectory(directory)
+    local success, results = pcall(os.execute, string.format(Helper.COMMAND.UNIVERSAL.CD, directory))
+    return success, result
+end
+
+function OsExecute.ExecuteInDirectory(directory, command)
+    if not directory or not command then
+        return false, "Paramètres manquants"
+    end
+    
+    local combined_cmd
+    if package.config:sub(1,1) == '\\' then
+        combined_cmd = string.format('cd /d "%s" && %s', directory, command)
+    else
+        combined_cmd = string.format('cd "%s" && %s', directory, command)
+    end
+    
+    local success, result = pcall(os.execute, combined_cmd)
+    return success, result
 end
 
 return OsExecute
--- core/os/helper.lua
+++ core/os/helper.lua
@@ -1,6 +1,20 @@
 return {
-    DEBIAN = {
-        COMMAND = "sudo apt update -y && sudo apt upgrade -y && sudo apt install -y %s && git submodule update --init",
-        PACKAGES = "git liblua5.4-dev screen build-essential libreadline-dev unzip clang cmake make gcc g++ libmysqlclient-dev libssl-dev libbz2-dev libreadline-dev libncurses-dev libboost-all-dev p7zip"
+    COMMAND = {
+        DEBIAN = {
+            UPDATE = "sudo apt update -y",
+            UPGRADE = "sudo apt upgrade -y",
+            INSTALL = "sudo apt install -y git liblua5.4-dev screen build-essential libreadline-dev unzip clang cmake make gcc g++ libmysqlclient-dev libssl-dev libbz2-dev libreadline-dev libncurses-dev libboost-all-dev p7zip",
+        },
+
+        LINUX = {
+            SUBMODULE = "git submodule update --init",
+            BUILD_LUAROCKS = "rm -rf build && mkdir build && ./configure --prefix=\"./build\" && make ${nproc} install",
+            SYM_LINK_LUAROCKS = "ln -s core/vendor/luarocks/build/bin/luarocks luarocks",
+            INSTALL_MOONSCRIPT = "./luarocks install moonscript"
+        },
+
+        UNIVERSAL = {
+            CD = "cd %s",
+        }
     }
 }
